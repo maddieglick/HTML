6ea9e4d8073e7acf00dcdb010ee56e67976f6cac
maddieglick <100810797+maddieglick@users.noreply.github.com>
2023-10-03 13:56:32 -0400

    Add files via upload
    got the buttons all to work, just need them to stay put when clicked, idk how to keep them aligned. added javascript file

---
+++ coffee_Java.js
@@ -0,0 +1,21 @@
+
+const toggleButtonLA = document.getElementById('toggleButtonLA');
+const listLA = document.getElementById('listLA');
+
+toggleButtonLA.addEventListener('click', () => {
+	listLA.classList.toggle('hidden');
+	});
+
+const toggleButtonA = document.getElementById('toggleButtonA');
+const listA = document.getElementById('listA');
+
+toggleButtonA.addEventListener('click', () => {
+	listA.classList.toggle('hidden');
+	});
+
+const toggleButtonI = document.getElementById('toggleButtonI');
+const listI = document.getElementById('listI');
+
+toggleButtonI.addEventListener('click', () => {
+	listI.classList.toggle('hidden');
+	});
